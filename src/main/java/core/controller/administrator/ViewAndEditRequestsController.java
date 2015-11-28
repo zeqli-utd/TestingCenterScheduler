019681d2526889e81b0f8b275fe40515cbeb5af9
zeqli <l.caecar@gmail.com>
2015-11-28 17:44:03 -0500

    1. Update ViewAndEditRequestsController

--- src/main/java/core/controller/administrator/ViewAndEditRequestsController.java
+++ src/main/java/core/controller/administrator/ViewAndEditRequestsController.java
@@ -1,20 +1,28 @@
 package core.controller.administrator;
 
+import core.event.dao.ExamDao;
 import core.event.dao.ReservationDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("admin/view-requests")
 public class ViewAndEditRequestsController {
     @Autowired
-    ReservationDao reservationDao;
+    ExamDao examDao;
 
+    /**
+     * Approve a request
+     * @param id
+     * @return
+     */
     @RequestMapping("approve/{id}")
     public String approveRequest(@PathVariable("id") String id) {
-
+        examDao.approveExam(id);
         return "redirect:admin/view-requests";
     }
+
 }
