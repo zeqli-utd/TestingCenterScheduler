9e3d39909af8ce84f4cf6d3a5e126dcb68f644b1
walt wu <wentao.wu@stonybrook.edu>
2015-11-29 18:19:48 -0500

    exam dao get pending proto

--- src/main/java/core/controller/instructor/ViewAppointmentsDetailController.java
+++ src/main/java/core/controller/instructor/ViewAppointmentsDetailController.java
@@ -9,16 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/instructor/view-appointmentsDetails")
+@RequestMapping("/instructor/view-appointments")
 public class ViewAppointmentsDetailController {
     @Autowired
     private AppointmentDao appointmentDao;
 
-    @RequestMapping("view-appointmentsDetail/{examId}")
+    @RequestMapping("view-appointments/{examId}")
     public ModelAndView viewRequests(@PathVariable("examId") String examId,
                                      ModelAndView model) {
-        model.setViewName("instructor/home");//find home.jsp-----view name
-        model.addObject("content", "instructor/include/view-appointmentsDetails");//"content" is in .jsp file
+        model.setViewName("instructor/home");
         model.addObject("heading", StringResources.INSTRUCTOR_VIEW_APPOINTMENTS_DETAIL);
         model.addObject("appointments", appointmentDao.findAllAppointmentsByExamId(examId));
         return model;
