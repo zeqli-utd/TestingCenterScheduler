bb7ff9c7217f86b86d5c038efea986ce2d785031
zeqli <l.caecar@gmail.com>
2015-11-28 19:31:35 -0500

    1. Add StudentEntry

---
+++ src/main/java/core/event/StudentEntry.java
@@ -0,0 +1,48 @@
+package core.event;
+
+
+import javax.persistence.Embeddable;
+
+/**
+ * This Class Hold Attendees information in Ad Hoc Exam
+ */
+@Embeddable
+public class StudentEntry {
+
+
+    String netId;
+
+    String firstName;
+
+    String lastName;
+
+    public StudentEntry() {
+
+    }
+
+    public String getNetId() {
+        return netId;
+    }
+
+    public void setNetId(String netId) {
+        this.netId = netId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+}
