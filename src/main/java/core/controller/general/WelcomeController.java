591a37419cfa9a750c1647eba49366183d79bef9
walt wu <wentao.wu@stonybrook.edu>
2015-11-30 00:25:18 -0500

    login

--- src/main/java/core/controller/general/WelcomeController.java
+++ src/main/java/core/controller/general/WelcomeController.java
@@ -1,6 +1,7 @@
 package core.controller.general;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,5 +16,16 @@ public class WelcomeController {
     @RequestMapping(value = "login", method = RequestMethod.GET)
     public String welcomeToLogin() {
         return "login";
+    }
+
+    @RequestMapping("login/error")
+    public String errorLogin(ModelMap modelMap) {
+        modelMap.addAttribute("errorMessage", "Please check login credentials");
+        return "login";
+    }
+
+    @RequestMapping("login/not-found")
+    public String errorNotFound() {
+        return "not-found";
     }
 }
